8388d0bb78d2f30a6dea0f54756d9733db4fbdf6
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2024-05-07 13:50:13 +0200

    Feed back utilisateur: affichages des messages d'erreurs et des anciennes données au niveau du formulaire d'inscription

--- components/input.php
+++ components/input.php
@@ -1,5 +1,5 @@
 <?php
-function html_input( string $type, string $placeholder, string $label, string $name) {
+function html_input( string $type, string $placeholder, string $label, string $name, string $value) : void {
     echo "
             <div class='my-4'>
                 <label class='text-xl' for='$name'>$label</label>
@@ -9,7 +9,13 @@ function html_input( string $type, string $placeholder, string $label, string $n
                      class='input input-bordered w-full'
                      name='$name'
                      id='$name'
+                     value='$value'
                 />
-            </div>
            ";
+    if(isset($_SESSION["errors"][$name])) {
+        $error_message = $_SESSION["errors"][$name];
+        echo "<p class='text-red-400'>$error_message</p>";
+    }
+
+    echo " </div>";
 }
--- pages/inscription.php
+++ pages/inscription.php
@@ -2,13 +2,34 @@
 <?php require_once "../components/input.php" ;?>
 <h1 class="text-4xl text-center">Page d'inscription</h1>
 
+<?php
+$http_method = $_SERVER["REQUEST_METHOD"];
+
+if($http_method === "POST") {
+    $uri = $_SERVER["REQUEST_URI"];
+    require_once "../validations/inscription-validator.php";
+    $data = inscription_validator();
+    if($data === null) {
+        header("Location:$uri");
+        exit();
+    }
+
+}
+
+?>
+
+
 <form class="w-2/3 mx-auto" action="" method="POST">
-    <?php html_input("text", "Ton prénom", "prénom", "firstname"); ?>
-    <?php html_input("text", "Ton nom", "nom", "lastname"); ?>
-    <?php html_input("email", "Ton email", "email", "email"); ?>
-    <?php html_input("password", "Ton mot de passe", "mot de passe", "password"); ?>
+    <?php html_input("text", "Ton prénom", "prénom", "firstname", $_SESSION["old"]["firstname"] ?? ''); ?>
+    <?php html_input("text", "Ton nom", "nom", "lastname",  $_SESSION["old"]["lastname"] ?? ''); ?>
+    <?php html_input("email", "Ton email", "email", "email",  $_SESSION["old"]["email"] ?? ''); ?>
+    <?php html_input("password", "Ton mot de passe", "mot de passe", "password", ''); ?>
     <button class="btn btn-secondary">Inscription</button>
 </form>
 
-<?php require_once "../partials/footer.php"; ?>
+<?php
+unset($_SESSION["errors"]);
+unset($_SESSION["old"]);
+require_once "../partials/footer.php";
+?>
 
